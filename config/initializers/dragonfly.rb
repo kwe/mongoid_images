348ae34991646f343271e898c6bcc1cdfaebd1e3
kwe <kwevans@gmail.com>
2010-12-19 22:30:32 +0000

    Add .jpg to generated files.
    Signed-off-by: kwe <kwevans@gmail.com>

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -9,8 +9,13 @@ db = YAML.load_file(Rails.root.join('config/mongoid.yml'))[Rails.env]['database'
 app.configure_with(:imagemagick)
 app.configure_with(:rails) do |c|
   c.datastore = Dragonfly::DataStorage::MongoDataStore.new :database => db
+  c.protect_from_dos_attacks = true
+  c.secret = 'duduhd dudgugsugd674683ydugdy8yw89t78e'
+  
 end
 
+app.url_suffix = '.jpg'
+# 
 # Allow all mongoid models to use the macro 'image_accessor'
 app.define_macro_on_include(Mongoid::Document, :image_accessor)
 
